a208163fee0d828d3b8706699995ce102585cfb1
Lilia <l.bekrar@ville-arles.fr>
2025-10-24 14:50:17 +0200

    changement UI bannière

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // ⚠ important : le slash AVANT et APRÈS le nom du repo
   base: '/profile-banner-generator/',
   plugins: [react()],
 })
